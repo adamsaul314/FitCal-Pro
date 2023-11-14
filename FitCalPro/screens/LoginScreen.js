83e448d9d701b0969ae3914bd68ac3e1e1f54193
S00197531 <S00197531@atu.ie>
2023-11-14 09:58:48 +0000

    login state

--- FitCalPro/screens/LoginScreen.js
+++ FitCalPro/screens/LoginScreen.js
@@ -1,21 +1,24 @@
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
         <TextInput
             placeholder='Email'
-            //value={ }
-            //onChangeText={text => }
+            value={email}
+            onChangeText={text => setEmail(text)}
             style={styles.input}
         />
 
         <TextInput
             placeholder='Password'
-            //value={ }
-            //onChangeText={text => }
+            value={password}
+            onChangeText={text => setPassword(text)}
             style={styles.input}
             secureTextEntry
         />
@@ -23,7 +26,7 @@ const LoginScreen = () => {
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={() => { }} style={styles.button}>
-          <Text style={styles.button}>Login</Text>
+          <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => { }} style={[styles.button, styles.buttonOutline]}>
