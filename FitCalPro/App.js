7d2501d97548a6c01b29a9673ee9af98b5d4597b
S00197531 <S00197531@atu.ie>
2023-11-16 14:22:44 +0000

    added login and tab navigation

--- FitCalPro/App.js
+++ FitCalPro/App.js
@@ -1,23 +1,85 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { initializeApp } from "firebase/app";
+// import { StyleSheet, Text, View } from 'react-native';
+// import app from './firebase';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import LoginScreen from './screens/LoginScreen';
+// import HomeScreen from './screens/HomeScreen';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// // const Tab = createBottomTabNavigator();
+
+// // function MyTabs() {
+// //   return (
+// //     <NavigationContainer>
+// //     <Tab.Navigator>
+// //       <Tab.Screen name="Login" component={LoginScreen} />
+// //       <Tab.Screen name="Home" component={HomeScreen} />
+// //     </Tab.Navigator>
+// //     </NavigationContainer>
+
+// //   );
+// // }
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//     <Stack.Navigator>
+//       <Stack.Screen name="Login" component={LoginScreen} />
+//       <Stack.Screen name="Home" component={HomeScreen} />
+
+//     </Stack.Navigator>
+//   </NavigationContainer>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 import { StatusBar } from 'expo-status-bar';
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { StyleSheet, Text, View } from 'react-native';
-import app from './firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import WorkoutScreen from './screens/WorkoutScreen';
+import DietScreen from './screens/DietScreen.js';
+import ProfileScreen from './screens/ProfileScreen';
+ import app from './firebase';
+
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Workout" component={WorkoutScreen} />
+      <Tab.Screen name="Diet" component={DietScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="HomeTabs" component={HomeTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- FitCalPro/screens/LoginScreen.js
+++ FitCalPro/screens/LoginScreen.js
@@ -1,100 +1,157 @@
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { initializeApp } from "firebase/app";
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useNavigation } from '@react-navigation/native';
+import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
+const Tab = createBottomTabNavigator();
+
 const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const auth = getAuth();
+      await signInWithEmailAndPassword(auth, email, password);
+      navigation.navigate('HomeTabs'); // Navigate to the tabs after successful login
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleRegister = async () => {
     try {
       const auth = getAuth();
       await createUserWithEmailAndPassword(auth, email, password);
-      // Handle successful registration, e.g., navigate to the login screen
+      navigation.navigate('HomeTabs'); // Navigate to the tabs after successful registration
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-            placeholder='Email'
-            value={email}
-            onChangeText={text => setEmail(text)}
-            style={styles.input}
+          placeholder='Email'
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
         />
 
         <TextInput
-            placeholder='Password'
-            value={password}
-            onChangeText={text => setPassword(text)}
-            style={styles.input}
-            secureTextEntry
+          placeholder='Password'
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
         />
-        
       </View>
+
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={() => { }} style={styles.button}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={handleRegister} style={[styles.button, styles.buttonOutline]}>
           <Text style={styles.buttonOutlineText}>Register</Text>
         </TouchableOpacity>
-
       </View>
-    </KeyboardAvoidingView>
-  )
-}
+    </View>
+  );
+};
 
-export default LoginScreen
+const HomeTab = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const WorkoutScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Workout Screen</Text>
+    </View>
+  );
+};
+
+const DietScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Diet Screen</Text>
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+};
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeTab} />
+      <Tab.Screen name="Workout" component={WorkoutScreen} />
+      <Tab.Screen name="Diet" component={DietScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    inputContainer: {
-      width: '80%'
-    },
-    input: {
-      backgroundColor: 'white',
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-      borderRadius: 10,
-      marginTop: 5,
-    },
-    buttonContainer: {
-      width: '60%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 40,
-    },
-    button: {
-      backgroundColor: '#0782F9',
-      width: '100%',
-      padding: 15,
-      borderRadius: 10,
-      alignItems: 'center',
-    },
-    buttonOutline: {
-      backgroundColor: 'white',
-      marginTop: 5,
-      borderColor: '#0782F9',
-      borderWidth: 2,
-    },
-    buttonText: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 16,
-    },
-    buttonOutlineText: {
-      color: '#0782F9',
-      fontWeight: '700',
-      fontSize: 16,
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
+
+export default LoginScreen;
