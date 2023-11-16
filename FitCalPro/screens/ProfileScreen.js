d8c958df3c3f85b0d4f3f521848ec09d86b2bf3d
S00197531 <S00197531@atu.ie>
2023-11-16 14:58:33 +0000

    added profile screen which displays logged in user

--- FitCalPro/screens/ProfileScreen.js
+++ FitCalPro/screens/ProfileScreen.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        navigation.navigate('Login'); // Redirect to login screen if not signed in
+      }
+    });
+
+    // Clean up subscription on unmount
+    return unsubscribe;
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome, {user?.email}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default ProfileScreen;
